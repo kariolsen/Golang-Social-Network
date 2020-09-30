4be6921fcd9636a101b6035976d72d8966d46c4c
Karin Olsen <deeeksha93@gmail.com>
2020-10-01 02:16:45 +0530

    Use a random generator to create session tokens
    Fixes #1

--- utils/sessions.go
+++ utils/sessions.go
@@ -1,26 +1,31 @@
 package utils
 
-import(
+import (
+	"crypto/rand"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 )
 
-var store = sessions.NewCookieStore([]byte("something-very-secret"))
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+var secretToken = generateRandomToken(10)
+var store = sessions.NewCookieStore([]byte(secretToken))
 
-func GetSession(c *gin.Context) *sessions.Session{ // return session info
+func GetSession(c *gin.Context) *sessions.Session { // return session info
 	session, err := store.Get(c.Request, "session") // Get the session info of this request
 	Err(err)
 	return session
 }
 
-func Get_Id_and_Username(c *gin.Context) (interface{}, interface{}){
+func Get_Id_and_Username(c *gin.Context) (interface{}, interface{}) {
 	session := GetSession(c)
 	id := session.Values["id"]
 	username := session.Values["username"]
 	return id, username
 }
 
-func Get_Avatar(c *gin.Context) (interface{}){
+func Get_Avatar(c *gin.Context) interface{} {
 	session := GetSession(c)
 	avatar := session.Values["avatar"]
 	return avatar
--- utils/utils.go
+++ utils/utils.go
@@ -10,3 +10,12 @@ func Err(err interface{}){
 	}
 }
 
+
+func generateRandomToken(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
